5c67d17a94a60e9e1a6899ad053290ee3415d2e2
mario21ic <Mario Inga Cahuana>
2019-03-09 01:16:43 -0500

    Ready

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
 var app = new Vue({
-  el: '#app',
+      el: '#app',
+      data: {
+         nombre: 'Lalo',
+         apellido: 'Landa',
+         numero: 0,
+         imagen_visible: true,
+         lista: [
+            {nombre: 'Lionel Messi', pais: 'Argentina'},
+            {nombre: 'Luis Suárez', pais: 'Uruguay'},
+            {nombre: 'Neymar', pais: 'Brasil'}, 
+            {nombre: 'James Rodríguez', pais: 'Colombia'},
+            {nombre: 'Andrés Iniesta', pais: 'España'},
+            {nombre: 'Chicharito Hernández', pais: 'México'}, 
+            {nombre: 'Alexis Sánchez', pais: 'Chile'},
+            {nombre: 'Paolo Guerrero', pais: 'Perú'},
+            {nombre: 'Paraguay', pais: 'Justo Villar'},
+            {nombre: 'Marcelo Moreno Martins', pais: 'Bolivia'},
+            {nombre: 'Walter Ayoví', pais: 'Ecuador'},
+            {nombre: 'Salomón Rondón', pais: 'Venezuela'},
+         ],
+         personajes: []
+      },
+      methods: {
+        generarNumeroAlAzar: function(){
+          this.numero = Math.floor((Math.random() * 1000) + 1);
+        },
+        created: function(){
+          this.$http.get('http://localhost:8080/demo').then(function(response){
+            console.log('Respuesta del servidor: ' + response);
+            this.personajes = response.body;
+          }, function(){
+            alert('Error!');
+          });
+        }
+      }
+   });
+
+var app1 = new Vue({
+  el: '#app-1',
     data: {
       message: 'Hello Vue!'
   }
